a0c91a21a2e3bfbe1ef6f3356041af23baaf728e
Eugene Petrenko <eugene.petrenko@gmail.com>
2013-05-14 23:56:49 +0200

    include time into report

--- android-navy/src/com/jonnyzzz/android/navy/ScanLog.java
+++ android-navy/src/com/jonnyzzz/android/navy/ScanLog.java
@@ -3,21 +3,28 @@ package com.jonnyzzz.android.navy;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
 * Created by Eugene Petrenko (eugene.petrenko@gmail.com)
 * Date: 14.05.13 23:35
 */
 public class ScanLog {
-  private final List<List<ScanItem>> myItems = new ArrayList<List<ScanItem>>();
+  private final long myStartTime = now();
+  private final Map<Integer, List<ScanItem>> myItems = new LinkedHashMap<Integer, List<ScanItem>>();
 
   public void addResults(@NotNull List<ScanItem> si) {
-    myItems.add(new ArrayList<ScanItem>(si));
+    myItems.put((int) (now() - myStartTime), new ArrayList<ScanItem>(si));
   }
 
   @NotNull
-  public List<List<ScanItem>> getItems() {
+  public Map<Integer, List<ScanItem>> getItems() {
     return myItems;
+  }
+
+  private static long now() {
+    return System.currentTimeMillis();
   }
 }
